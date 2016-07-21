3955dc1f9fe63b982bb07fc87c41e833419f7da3
ashes999 <ashes999@yahoo.com>
2016-07-21 17:29:24 -0400

    Fixes for embedding Warnings/ignores aren't hammered any more.

--- src/app_builder.py
+++ src/app_builder.py
@@ -37,8 +37,10 @@ class AppBuilder:
         source_relative_directory = "{0}/".format(os.path.realpath(watch_path))
         output_relative_directory = "{0}/".format(os.path.realpath(output_directory))
         
-        class_regex = re.compile(AppBuilder.CLASS_REGEX, re.IGNORECASE)
-        
+        class_regex = re.compile(AppBuilder.CLASS_REGEX, re.IGNORECASE) # compiled so it's faster
+        # when we embed, we copy all files, but delete non-modules from bin. Don't keep doing that
+        # every second; remember and stop trying to copy these over.
+        module_files_to_ignore = set({}) 
         print("Monitoring {0} to build to {1}".format(os.path.realpath(watch_path), output_directory))
         
         while True:
@@ -48,9 +50,12 @@ class AppBuilder:
             template_files = io.directory.traverse_for_mtime(relative_template_directory, {}, None, "{0}/".format(os.path.realpath(relative_template_directory)))
             
             # Collections of File instances
-            added_files = [source_files[f] for f in source_files if not f in destination_files]
-            changed_files = [source_files[f] for f in source_files if destination_files.has_key(f) and source_files[f].mtime != destination_files[f].mtime]
-            deleted_files = [destination_files[f] for f in destination_files if not f in source_files and not f in template_files]
+            added_files = {source_files[f] for f in source_files if not f in destination_files}
+            changed_files = {source_files[f] for f in source_files if destination_files.has_key(f) and source_files[f].mtime != destination_files[f].mtime}
+            deleted_files = {destination_files[f] for f in destination_files if not f in source_files and not f in template_files}
+
+            # Don't treat module_files_to_ignore as new files
+            added_files = {f for f in added_files if not f.full_path in module_files_to_ignore}
             
             with open("{0}/{1}/{2}".format(AppBuilder.TEMPLATE_DIRECTORY, AppBuilder.JABAL_BACKEND, AppBuilder.MAIN_HTML_FILE)) as template_file:
                 original = template_file.read()
@@ -60,6 +65,8 @@ class AppBuilder:
             if len(added_files) > 0:
                 rebuild = True
                 for file in added_files:
+                    if file.full_path in module_files_to_ignore:
+                        continue
                     io.directory.safe_copy(file, source_relative_directory, output_directory, "new")
 
             if len(changed_files) > 0:
@@ -73,34 +80,42 @@ class AppBuilder:
                     os.remove(file.full_path)
                     print("Deleted bin-only file {0}".format(file.relative_path(output_relative_directory)))                    
                 
-            if AppBuilder.VALID_COMMAND_LINE_ARGUMENTS["embed code"] in cmdline_arguments and rebuild == True:
-                # We may have copied over python files that are not modules. These
-                # shouldn't be copied over. Nuke them if that's the case.
-                for file in added_files.values():
-                    if file.filename.endswith('.py'):
-                        with open(file) as python_file:
-                            contents = python_file.read()
-                        if class_regex.match(contents):
-                            # It's not a module. Nuke it!
-                            os.remove(file.fullpath)
-                            print("Deleted {0} because it was copied over but isn't a module".format(file.filename))                                                      
-                
-                with open(main_file) as source_file:
-                    main_code = source_file.read()
+            if rebuild == True:
+                if AppBuilder.VALID_COMMAND_LINE_ARGUMENTS["embed code"] in cmdline_arguments:
+                    # We may have copied over python files that are not modules. These
+                    # shouldn't be copied over. Nuke them if that's the case.
+                    for file in added_files:
+                        if file.full_path in module_files_to_ignore:
+                            continue
+                            
+                        if file.filename.endswith('.py'):
+                            with open(file.full_path) as python_file:
+                                contents = python_file.read()
+                            if class_regex.match(contents):
+                                # It's not a module. Nuke it!
+                                destination = "{0}/{1}".format(output_directory, file.relative_path(source_relative_directory))
+                                os.remove(destination)
+                                print("Deleted {0} because it was copied over but isn't a module".format(destination))
+                                module_files_to_ignore.add(file.full_path) 
                     
-                jabal_module_file = "{0}/{1}".format(relative_template_directory, AppBuilder.JABAL_MAIN_PY)
-                with open(jabal_module_file) as jabal_module:
-                    jabal_code = jabal_module.read()
-                    main_code = "{0}\r\n\r\n\r\n{1}".format(jabal_code, main_code)
+                    with open(main_file) as source_file:
+                        main_code = source_file.read()
+                        
+                    jabal_module_file = "{0}/{1}".format(relative_template_directory, AppBuilder.JABAL_MAIN_PY)
+                    with open(jabal_module_file) as jabal_module:
+                        jabal_code = jabal_module.read()
+                        main_code = "{0}\r\n\r\n\r\n{1}".format(jabal_code, main_code)
+                        
+                    main_code = self.inline_imports(watch_path, main_code)
+                else:
+                    main_code = original.replace(AppBuilder.CONTENT_PLACEHOLDER, AppBuilder.JABAL_MAIN_CODE)
                     
-                main_code = self.inline_imports(watch_path, main_code)
-            else:
-                main_code = original.replace(AppBuilder.CONTENT_PLACEHOLDER, AppBuilder.JABAL_MAIN_CODE)
-                
-            with open("{0}/{1}".format(output_directory, AppBuilder.MAIN_HTML_FILE), 'w+') as out_file:
-                substituted = original.replace(AppBuilder.CONTENT_PLACEHOLDER, main_code)
-                out_file.write(substituted)
+                out_file_name = "{0}/{1}".format(output_directory, AppBuilder.MAIN_HTML_FILE)
+                with open(out_file_name, 'w+') as out_file:
+                    substituted = original.replace(AppBuilder.CONTENT_PLACEHOLDER, main_code)
+                    out_file.write(substituted)
                     
+                print("Rebuilt {0} at {1}".format(out_file_name, datetime.datetime.now()))
             time.sleep(0.5)
             
     def validate_args(self, args):
