661e09d1e4acccee0b63c1fe5f34c58a71822661
ashes999 <ashes999@yahoo.com>
2016-07-22 23:35:11 -0400

    Partially moved over init

---
+++ template/melonjs/jabal.py
@@ -0,0 +1,8 @@
+from browser import window
+global melonjs
+melonjs = window.me
+
+#def init(width, height):
+#    settings = { "wrapper": "screen", "scale": "auto" }
+#    if (not melonjs.video.init(1024, 768, settings)):
+#        window.alert("Your browser does not support HTML5 canvas.")
