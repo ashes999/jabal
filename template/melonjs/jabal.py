5c4fa223140811cce7ebaf4aabc95c4ea04b43a5
ashes999 <ashes999@yahoo.com>
2016-07-24 07:57:30 -0400

    Making progress on getting a screen out.

--- template/melonjs/jabal.py
+++ template/melonjs/jabal.py
@@ -1,42 +1,34 @@
 from browser import window
 from browser import console
-from browser import document
+import javascript
 
 global melonjs
 melonjs = window.me
 
-def load_script(path):
-    el = document.createElement('script')
-    el.async = False
-    el.src = path
-    el.type = 'text/javascript'
-
-    apply_to = document.body #document.getElementsByTagName('HEAD')[0]
-    apply_to.appendChild(el)
-
-def init(width, height, resources):
-    game = Game(width, height, resources)
+def init(width, height, resources, starting_screen):
+    game = Game(width, height, resources, starting_screen)    
     # We have to allow JS to interact with this object for some operations
     # eg. assigning resources automatically from resources.js
     window.game = game
 
     # Game Scripts. Load after Brython so window.game is defined.
     # Plugins
-    load_script("lib/debugPanel.js")
+    javascript.load("lib/debugPanel.js")
     # Content
-    load_script("js/entities/entities.js")    
-    load_script("js/entities/HUD.js")
-    load_script("js/screens/play.js")
+    javascript.load("js/entities/entities.js")    
+    javascript.load("js/entities/HUD.js")
+    javascript.load("js/screens/play.js")
     
     game.onload()
 
 class Game:
-    def __init__(self, width, height, resources):
+    def __init__(self, width, height, resources, starting_screen):
         self.data = { "score" : 0 }        
         
         self.width = width
         self.height = height
         self.resources = resources
+        self.starting_screen = starting_screen
 
     def onload(self):
         # we don't need the default 60fps for a whack-a-mole !
@@ -54,17 +46,13 @@ class Game:
         # set all ressources to be loaded
         # melonjs.loader.preload(self.resources, self.loaded.bind(self.onload))
         melonjs.loader.preload(self.resources, self.loaded)
-        console.log(self.width)
-        console.log(self.height)
-        console.log(self.resources)
         
     def loaded(self): 
         # set the "Play/Ingame" Screen Object
-        melonjs.state.set(melonjs.state.PLAY, PlayScreen())
+        melonjs.state.set(melonjs.state.PLAY, self.starting_screen)
 
         # set a fade transition effect
         melonjs.state.transition("fade","#000000", 250)
 
         # start the game
         melonjs.state.change(melonjs.state.PLAY)
-        
